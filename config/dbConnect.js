b31c0a86fbbcc06de7e6be4d686b9aea449ba552
dustein <steindu@gmail.com>
2022-03-25 13:38:40 -0300

    adicionado uso mongodb local

--- config/dbConnect.js
+++ config/dbConnect.js
@@ -1,10 +1,15 @@
-import dotenv from 'dotenv';
-dotenv.config();
+// import dotenv from 'dotenv';
+// dotenv.config();
 import mongoose from 'mongoose';
 
-const URL = process.env.MONGO_URI;
-mongoose.connect(URL, { useNewUrlParser: true, useUnifiedTopology: true})
+// const URL = process.env.MONGO_LOCAL;
+// mongoose.connect(URL, { useNewUrlParser: true, useUnifiedTopology: true})
+
+//config uso MongoDB local
+mongoose.connect('mongodb://localhost/livraria')
+//config uso MongoDB Atlas
+// mongoose.connect("mongodb+srv://dustein:data02@cluster0.5iskh.mongodb.net/livraria")
 
 let db = mongoose.connection;
 
-export default db
+export default db;
